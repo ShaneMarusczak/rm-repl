3ed81290cd931bc74d82857d3c082996ab3bc27f
ShaneMarusczak <marusczaks@gmail.com>
2022-11-23 13:33:59 -0500

    plotting works with graplot

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use rusty_maths::equation_analyzer::calculator::calculate;
+use graplot::Plot;
+use rusty_maths::equation_analyzer::calculator::{calculate, plot};
 use std::io::{self, Write};
 use std::process::exit;
 
 mod repl;
+use repl::PreviousAnswer;
 use repl::Repl;
 
 fn main() {
@@ -27,8 +29,9 @@ fn main() {
             continue;
         }
 
-        if line_trim.eq("q") {
-            exit(0);
+        if let Some(stripped) = line_trim.strip_prefix(':') {
+            run_command(stripped, &mut repl);
+            continue;
         }
 
         let ans_requested = line_trim.contains("ans");
@@ -48,12 +51,70 @@ fn main() {
 fn run(line: &str, repl: &mut Repl) {
     let val = calculate(line);
     if let Ok(v) = val {
-        repl.previous_answer = v;
-        repl.previous_answer_valid = true;
+        repl.previous_answer(v, true);
         println!("{}", v);
     } else {
-        repl.previous_answer = 0.0;
-        repl.previous_answer_valid = false;
+        repl.previous_answer(0.0, false);
         eprintln!("{}", val.unwrap_err());
+    }
+}
+
+fn run_command(line: &str, repl: &mut Repl) {
+    match line {
+        "q" => exit(0),
+        "p" => p(),
+        _ => {
+            eprintln!("Invalid Command");
+            repl.previous_answer(0.0, false);
+        }
+    }
+}
+
+fn p() {
+    print!("Equation: ");
+    io::stdout().flush().unwrap_or_default();
+    let mut eq = String::new();
+    io::stdin().read_line(&mut eq).expect("Failed to read line");
+
+    print!("x min: ");
+    io::stdout().flush().unwrap_or_default();
+    let mut x_min = String::new();
+    io::stdin()
+        .read_line(&mut x_min)
+        .expect("Failed to read line");
+
+    print!("x max: ");
+    io::stdout().flush().unwrap_or_default();
+    let mut x_max = String::new();
+    io::stdin()
+        .read_line(&mut x_max)
+        .expect("Failed to read line");
+
+    print!("step size: ");
+    io::stdout().flush().unwrap_or_default();
+    let mut step_size = String::new();
+    io::stdin()
+        .read_line(&mut step_size)
+        .expect("Failed to read line");
+
+    let points = plot(
+        eq.trim(),
+        x_min.trim().parse::<f32>().unwrap(),
+        x_max.trim().parse::<f32>().unwrap(),
+        step_size.trim().parse::<f32>().unwrap(),
+    );
+
+    let mut xs = vec![];
+    let mut ys = vec![];
+
+    if let Ok(points) = points {
+        for point in points {
+            xs.push(point.0);
+            ys.push(point.1);
+        }
+        let mut plot = Plot::new((xs, ys));
+        plot.set_title(&eq);
+        plot.show();
+        println!("got here");
     }
 }
--- src/repl.rs
+++ src/repl.rs
@@ -2,3 +2,14 @@ pub(crate) struct Repl {
     pub(crate) previous_answer: f32,
     pub(crate) previous_answer_valid: bool,
 }
+
+pub(crate) trait PreviousAnswer {
+    fn previous_answer(&mut self, value: f32, valid: bool);
+}
+
+impl PreviousAnswer for Repl {
+    fn previous_answer(&mut self, value: f32, valid: bool) {
+        self.previous_answer = value;
+        self.previous_answer_valid = valid;
+    }
+}
