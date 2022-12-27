2ff5c3a795e2eb007acb151e8346aa427fa0078b
ShaneMarusczak <marusczaks@gmail.com>
2022-12-26 19:46:34 -0500

    chatGPT rewrite

--- src/variables/mod.rs
+++ src/variables/mod.rs
@@ -24,22 +24,24 @@ pub(crate) fn insert_ans_vars(str: &str, repl: &repl::Repl) -> String {
         str = str.replace("ans", &repl.previous_answer.to_string());
     }
 
-    if !repl.variables.is_empty() {
-        repl.variables
-            .iter()
-            .for_each(|(from, to)| str = str.replace(*from, to));
+    for (from, to) in &repl.variables {
+        str = str.replace(*from, to);
     }
+
     str
 }
 
 pub(crate) fn is_variable(str: &str) -> bool {
     let str = str.to_owned();
-    if str.len() < 2 || !str.starts_with(|c: char| c.is_alphabetic() && c.is_uppercase()) {
-        return false;
+    match (
+        str.len() >= 2,
+        str.starts_with(char::is_alphabetic) && str.starts_with(char::is_uppercase),
+    ) {
+        (true, true) => match str.chars().nth(1) {
+            Some('=') => true,
+            Some(' ') => str.chars().nth(2) == Some('='),
+            _ => false,
+        },
+        _ => false,
     }
-    let second_is_equal = str.chars().nth(1).unwrap().eq(&'=');
-    let second_is_space_and_third_char_is_equal_sign =
-        str.chars().nth(1).unwrap().eq(&' ') && str.chars().nth(2).unwrap().eq(&'=');
-
-    second_is_equal || second_is_space_and_third_char_is_equal_sign
 }
